547a2d093e331db3d8176ab74b0cde988f612847
Dipankar Saha <dipankarsaha1059@gmail.com>
2021-02-12 12:21:54 +0600

    updated

--- src/TestDkSdk.php
+++ src/TestDkSdk.php
@@ -18,37 +18,8 @@ class TestDkSdk
     }
 
     public function getExchangeRate($from, $to) {
-        /*$curl = curl_init();
-
-        curl_setopt_array($curl, array(
-          CURLOPT_URL => $this->baseUrl."/api/".$this->apiVersion."/exchange-rate",
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => "",
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 0,
-          CURLOPT_FOLLOWLOCATION => true,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => "POST",
-          CURLOPT_POSTFIELDS => "from=$from&to=$to",
-          CURLOPT_HTTPHEADER => array(
-            "Content-Type: application/x-www-form-urlencoded",
-            "Accept: application/json",
-            "Accept-Language: en",
-            "x-auth: ".$this->publicKey,
-          ),
-        ));
-
-        $response = json_decode(curl_exec($curl));
-
-        curl_close($curl);
-
-        return $response;*/
-
         $url = $this->baseUrl.'/api/'.$this->apiVersion.'/exchange-rate';
-        $data = [
-            'from' => $from,
-            'to'=> $to
-        ];
+        $data = "from=$from&to=$to";
         return $this->callCurl($url, 'POST', $data);
     }
 
@@ -57,7 +28,7 @@ class TestDkSdk
         return $this->callCurl($url, 'GET');
     }
 
-    private function callCurl($url, $method, $data = []) {
+    private function callCurl($url, $method, $data = '') {
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
@@ -79,9 +50,7 @@ class TestDkSdk
         ));
 
         $response = json_decode(curl_exec($curl));
-
         curl_close($curl);
-
         return $response;
     }
 }
