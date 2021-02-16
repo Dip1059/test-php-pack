bb7224f37dbec97a74705555b0ae11552e1f0cc2
Dipankar Saha <dipankarsaha1059@gmail.com>
2021-02-16 13:17:01 +0600

    updated

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,12 +3,41 @@
 namespace Test\TestDkSdk\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Test\TestDkSdk\TestDkSdk;
 
 class ExampleTest extends TestCase
 {
-    /** @test */
-    public function true_is_true()
+    private $collpay;
+    public function setUp()
     {
-        $this->assertTrue(true);
+        parent::setUp();
+        $this->collpay = new TestDkSdk('xxxxxxxxxxxxxx', COLLPAY_ENV_SANDBOX);
+    }
+
+    public function test_env_production_will_be_1() {
+        $this->assertEquals(1, COLLPAY_ENV_PRODUCTION);
+    }
+
+    public function test_env_sandbox_will_be_2() {
+        $this->assertEquals(2, COLLPAY_ENV_SANDBOX);
+    }
+
+    public function test_collpay_v1_will_be_v1() {
+        $this->assertEquals('v1', COLLPAY_V1);
+    }
+
+    public function test_getExchangeRate_basic_error() {
+        $response = $this->collpay->getExchangeRate('USD', 'BTC');
+        $this->assertEquals(false, $response->success);
+    }
+
+    public function test_createTransaction_basic_error() {
+        $response = $this->collpay->createTransaction([]);
+        $this->assertEquals(false, $response->success);
+    }
+
+    public function test_getTransaction_basic_error() {
+        $response = $this->collpay->getTransaction('xxxxxxxxxxxxxx');
+        $this->assertEquals(false, $response->success);
     }
 }
